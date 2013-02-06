227891fd6d63800c8b959b147f24758687d26332
Joey Maloney <jmalone4@ycp.edu>
2013-02-06 09:47:21 -0500

    fdddd

--- Calculator/src/edu/ycp/cs320/calculator/Main.java
+++ Calculator/src/edu/ycp/cs320/calculator/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the calculator!");
 		System.out.println("Yeah, this is a great program");
+		System.out.println("Welcome to the calculator!");
 	}
 }
 
